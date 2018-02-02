74fa2641ae04001ab428db967aefe3851776e368
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-02-02 09:03:29 -0600

    Implemented `CleanScanner`
    Avoids inputs that crash program

---
+++ src/daily/day05/IfElseDemo.java
@@ -0,0 +1,29 @@
+package daily.day05;
+
+import experiments.CleanScanner;
+import java.util.Scanner;
+
+public class IfElseDemo
+{
+    static Scanner scanner = new Scanner(System.in);
+    static String name;
+    static int age;
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello! What is your name?");
+        name = scanner.nextLine();
+        age = CleanScanner.cleanNextInt(("Alright, " + name + ". How old are you?"), scanner);
+        giveAdvice(age, name);
+    }
+
+    private static void giveAdvice(int age, String name)
+    {
+        if (age <= 15)
+            System.out.println("Ha, you're barely considered a citizen!");
+        else if (age <= 17)
+            System.out.println("How's the drivers license treating you, " + name + "?");
+        else if (age >= 18)
+            System.out.println("Brush up on your politics, you'll be voting in the next election!");
+    }
+}
