f6b9e3d422aa7859e04ba8b9c159650427cdb7d9
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-02-06 09:09:58 -0600

    Overloading of CleanScanner
    Don't want to print the prompt every time? Think your user is smart enough to remember the prompt after failing to enter an integer 900 times? No problem.

--- src/experiments/CleanScanner.java
+++ src/experiments/CleanScanner.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class CleanScanner
 {
+    public static int cleanNextInt(Scanner scanner)
+    {
+        if (scanner.hasNextInt())
+        {
+            return scanner.nextInt();
+        }
+
+        else
+        {
+            System.out.println("Please enter an integer.");
+            scanner.nextLine();
+            return cleanNextInt(scanner);
+        }
+    }
 
     public static int cleanNextInt(String prompt, Scanner scanner)
     {
